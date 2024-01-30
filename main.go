caa61616b229132203a5d0ce21afeb14624fc420
ArkjuniorK <created.arkjuniork@gmail.com>
2024-01-30 09:25:14 -0500

    finishing up

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"path"
@@ -12,7 +11,10 @@ import (
 	"github.com/gocolly/colly"
 )
 
-var url = flag.String("url", "https://telegram.org", "URL to crawl")
+var (
+	url = flag.String("url", "https://telegram.org", "URL to crawl")
+	wpt = ""
+)
 
 func main() {
 	flag.Parse()
@@ -22,26 +24,71 @@ func main() {
 		log.Panicln(errors.New("crawler: empty url"))
 	}
 
+	domain := getDomain(*url)
+	domains := getDomains(*url)
+
 	wd, err := os.Getwd()
 	if err != nil {
 		log.Panicln(err)
 	}
 
-	webpath := path.Join(wd, "result", *url)
-	file, err := os.Create(webpath)
-	if err != nil {
+	wpt = path.Join(wd, "result", domain)
+	if err := handleDir(&wpt); err != nil {
 		log.Panicln(err)
 	}
 
-	c := colly.NewCollector(colly.AllowedDomains(getDomain(*url)))
-	crawlWebsite(c, *url, file)
+	c := colly.NewCollector(
+		colly.AllowedDomains(domains...),
+	)
+
+	start(c, url, &wpt)
 }
 
-func crawlWebsite(c *colly.Collector, url string, file *os.File) {
-	log.Println("Start crawling", url)
-	defer log.Println("Result saved at", file.Name())
+func start(c *colly.Collector, url, wpt *string) {
+	log.Println("Start crawling", *url)
+	defer log.Println("Result saved at", *wpt)
 
 	c.OnRequest(func(r *colly.Request) {
+		up := r.URL.Path
+		if len(up) == 0 {
+			return
+		}
+
+		log.Println("Create dir for path", up)
+		cp := path.Join(*wpt, up)
+		if err := handleDir(&cp); err != nil {
+			log.Println("An error has occured, aborting")
+			log.Panicln(err)
+		}
+	})
+
+	c.OnHTML("html", func(h *colly.HTMLElement) {
+		cp := path.Join(*wpt, "index.html")
+
+		up := h.Request.URL.Path
+		if len(up) != 0 {
+			p := path.Join(*wpt, h.Request.URL.Path)
+			if err := handleDir(&p); err != nil {
+				log.Println("An error has occured, aborting")
+				log.Panicln(err)
+			}
+
+			cp = path.Join(*wpt, h.Request.URL.Path, "index.html")
+
+			c.OnHTML("a[href]", func(h *colly.HTMLElement) {
+				link := h.Attr("href")
+				h.Request.Visit(link)
+			})
+			c.OnHTML("a[href]", func(h *colly.HTMLElement) {
+				link := h.Attr("href")
+				h.Request.Visit(link)
+			})
+		}
+
+		if err := h.Response.Save(cp); err != nil {
+			log.Println("An error has occured, aborting")
+			log.Panicln(err)
+		}
 	})
 
 	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
@@ -49,23 +96,47 @@ func crawlWebsite(c *colly.Collector, url string, file *os.File) {
 		h.Request.Visit(link)
 	})
 
-	c.OnHTML("html", func(h *colly.HTMLElement) {
-		fmt.Println(h.Request.URL, url)
-	})
-
 	c.OnError(func(rs *colly.Response, err error) {
-		log.Println("An error has occured, aborting")
-		log.Println("Result saved at", file.Name())
-		log.Panicln(err)
+		log.Println(
+			"An error has occured for path",
+			rs.Request.URL.Path,
+			"with", err,
+			"skipping!",
+		)
 	})
 
-	c.Visit(url)
-
+	c.Visit(*url)
 }
 
 func getDomain(url string) string {
 	u := strings.Split(url, "//")[1]
-	d := strings.Split(u, "/")[0]
+	return strings.Split(u, "/")[0]
+}
 
-	return d
+func getDomains(url string) []string {
+	var (
+		d       = getDomain(url)
+		domains = make([]string, 0)
+	)
+
+	for _, v := range []string{"", "www."} {
+		domains = append(domains, v+d)
+	}
+
+	return domains
+}
+
+func handleDir(p *string) error {
+	err := os.MkdirAll(*p, os.ModePerm)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		return err
+	}
+
+	if errors.Is(err, os.ErrExist) {
+		if err = os.RemoveAll(*p); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
